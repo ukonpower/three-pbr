0b9b2d715ef145a5274c6e438787ee88d015e558
ryohei ukon <obshi0120@icloud.com>
2020-04-02 11:58:24 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: ''
+    },
+    output: {
+        filename: ''
+    },
+    module: {
+        rules: [{
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.(glsl|vs|fs)$/,
+                loader: 'shader-loader',
+                options: {
+                    glsl: {
+                        chunkPath: './src/glsl-chunks'
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    }
+};
